89846149eeb4eef1c2ec7af3c5b22ef670807bc2
안광수 <jhk7716@gmail.com>
2021-04-27 17:57:45 +0900

    임시저장 2

--- src/router/service/index.js
+++ src/router/service/index.js
@@ -1,10 +1,15 @@
 import Benefit from '@/components/creator/Benefit'
-
+import Partners from '@/components/partners/Partners'
 
 export default [
     {
         path:'/creator/benefit',
         name:'Benefit',
         component:Benefit
+    },
+    {
+        path:'/partners',
+        name:'Partners',
+        component:Partners
     }
 ]
